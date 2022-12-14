20a84a30c8c9a0f99abd65b4598f5e07d8d6c924
noceive <nocivescience@gmail.com>
2022-12-14 00:26:16 -0300

    funciona para el segundo video

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const tituloEl=document.getElementById('titulo');
 const titulo='Tetris'
+const Cols=20;
+const Rows=30;
+const scaleGame=32;
 tituloEl.innerHTML=`
     Juego de ${titulo}
 `;
@@ -15,14 +18,52 @@ const color=[
     'red',
 ]
 class PieceRender{
-    constructor(){
+    constructor(shape,ctx){
+        this.shape=shape;
+        this.ctx=ctx
+        this.x=5;
+        this.y=7;
+    }
+    rendering(){
+        this.shape.forEach((row,i)=>{
+            row.forEach((cell,j)=>{
+                if(cell!==0){
+                    ctx.fillStyle='red';
+                    ctx.fillRect(this.x+j,this.y+i,1,1)
+                }
+            })
+        })
     }
 }
-function newGameState(){
-    const gameCanvas=document.getElementById('tetris')
-    const ctx=gameCanvas.getContext('2d')
+class GameModel{
+    constructor(ctx){
+        this.ctx=ctx;
+        this.fallingPiece=null;
+        this.grid=this.MakeStartingGrid();
+    }
+    MakeStartingGrid(){
+        let grid=[]
+        for(let i=0;i<Rows;i++){
+            grid.push([]);
+            for(let j=0;j<Cols;j++){
+                grid[grid.length-1].push(0)
+            }
+        }
+        return grid;
+    }
 }
-const piece= new PieceRender()
+const canvas=document.getElementById('tetris');
+const ctx=canvas.getContext('2d');
+ctx.scale(scaleGame,scaleGame)
+const model=new GameModel(ctx)
+function newGameState(){
+    if(model.fallingPiece===null){
+        const piece=new PieceRender(shapes[0],ctx)
+        model.fallingPiece=piece
+        model.fallingPiece.rendering();
+    }
+}
+newGameState()
 document.addEventListener('keydown',function(e){
     switch(e.key){
         case 'a':
@@ -37,7 +78,5 @@ document.addEventListener('keydown',function(e){
         case 'w':
             console.log('rotar')
             break;
-        case 'p':
-            console.log(piece.ejemplo)
     }
 })
